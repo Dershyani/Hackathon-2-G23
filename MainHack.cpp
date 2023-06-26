0b971886d47f470a142f1c36d2f14e18c524e891
Jashh18 <118554140+Jashh18@users.noreply.github.com>
2023-06-27 03:41:34 +0800

    Update MainHack.cpp

--- MainHack.cpp
+++ MainHack.cpp
@@ -9,7 +9,10 @@ using namespace std;
 
 int main()
 {
-	char newOrResume;
+	ofstream of;
+	fstream F;
+	char newOrResume, stop;
+	string name, category, line;
 	FoodList f;
 	CountryList c;
 	CelebrationList cl;
@@ -18,7 +21,10 @@ int main()
 	string output;
 	
 	//start game
-	cout << "Do you want to play a game(y/n): ";
+	cout << "What is your name warrior: ";
+	cin.ignore();
+	getline(cin, name);
+	cout << "Do you want to play a new game(y) or resume a game(n): ";
 	cin>>newOrResume;
 	
 	while(newOrResume=='y' || newOrResume=='Y')
@@ -34,7 +40,7 @@ int main()
 		c.appendNode("malaysia");
 		c.appendNode("america");
 		c.appendNode("india");
-		c.appendNode("singapore");
+		c.appendNode("japan");
 		c.appendNode("china");
 		
 		//celebration
@@ -45,250 +51,234 @@ int main()
 		cl.appendNode("thaipusam");
 		
 		//food game
-	    string a_food[5] = { "apple","pizza","sandwich","chicken","rice" };
-	    char arr_food[10][10];
-	
-		for(int b=0; b<5; b++)
-		{
-		    for (int i = 0; i < 10; i++) {
-		        for (int j = 0; j < 10; j++) {
-		            char c;
-		            int r;
-		            r = rand() % 26;   // generate a random number
-		            c = 'a' + r;            // Convert to a character from a-z
-		            arr_food[i][j] = c;
-		        }
-		    }
-		
-		    int index = rand() % 5;
-		    output = a_food[index];
-		
-		    if (a_food[index].length() % 2 == 0) {
-		        // for even string length case
-		        int row = rand() % 10;
-		        int col = rand() % 3;
-		        for (int i = 0; a_food[index][i] != '\0'; i++, col++) {
-		            arr_food[row][col] = a_food[index][i];
-		        }
-		    }
-		    else {
-		        // for odd string length case
-		        int row = rand() % 3;
-		        int col = rand() % 10;
-		        for (int i = 0; a_food[index][i] != '\0'; i++, row++) {
-		            arr_food[row][col] = a_food[index][i];
-		        }
-		    }
-		
-		    for (int i = 0; i < 10; i++) {
-		        for (int j = 0; j < 10; j++) {
-		            cout << " " << arr_food[i][j];
-		            Sleep(90);
-		        }
-		        cout << endl;
-		    }
-		
-		    string guess;
-	        cout << "Press 1 for Guess" << endl;
-		    cout << "Press 2 for see Hint and then guess" << endl;
-		    cout << "Enter input		:	";
-		    cin >> hint;
-		    cout << endl;
-		
-		    if (hint == 1) {
-		        cout << "Enter word you guess         :       ";
-		        cin.ignore();  // Ignore any previous newline character in the input buffer
-		        getline(cin, guess);
-		    }
-		    else if (hint == 2) {
-		        cout << "Guessed word in puzzle size is  :       ";
-		        cout << output.size();
-		        cout << endl;
-		        cout << "Enter word you guess         :       ";
-		        cin.ignore();  // Ignore any previous newline character in the input buffer
-		        getline(cin, guess);
-		    }
-		    else {
-		        cout << "Invalid input	";
-		        return 0;  // Exit the function since the input is invalid
-		    }
-		
-		    cout << endl;
-		    if (f.searchNode(guess)) 
-			{
-		        cout << "Congratulations! You have guessed the correct word." << endl;
-		        scores += 2;
-		    }
-		    else {
-		        cout << "Wrong guess. The correct word was: " << output << endl;
-		    }
-		    cout << endl;
+	    f.foodGame(scores);
+	    category = "food";
+	    cout << "Do you want to stop the game? (y/n): ";
+	    cin >> stop;
+	    if(stop=='Y' || stop=='y')
+	    {
+	    	of.open("text.txt", ios::app);
+	    	of << name << endl;
+	    	of << category << endl;
+	    	of << scores << endl;
+	    	cout << "Progress saved.";
+	    	of.close();
+	    	break;	    	
 		}
-		
-		//country game
-	    string a_country[5] = { "malaysia","america","singapore","india","china" };
-	    char arr_country[10][10];
-	
-		for(int b=0; b<5; b++)
+		else if(stop=='N' || stop=='n')
 		{
-		    for (int i = 0; i < 10; i++) {
-		        for (int j = 0; j < 10; j++) {
-		            char c;
-		            int r;
-		            r = rand() % 26;   // generate a random number
-		            c = 'a' + r;            // Convert to a character from a-z
-		            arr_country[i][j] = c;
-		        }
-		    }
-		
-		    int index = rand() % 5;
-		    output = a_country[index];
-		
-		    if (a_country[index].length() % 2 == 0) {
-		        // for even string length case
-		        int row = rand() % 10;
-		        int col = rand() % 3;
-		        for (int i = 0; a_country[index][i] != '\0'; i++, col++) {
-		            arr_country[row][col] = a_country[index][i];
-		        }
-		    }
-		    else {
-		        // for odd string length case
-		        int row = rand() % 3;
-		        int col = rand() % 10;
-		        for (int i = 0; a_country[index][i] != '\0'; i++, row++) {
-		            arr_country[row][col] = a_country[index][i];
-		        }
-		    }
-		
-		    for (int i = 0; i < 10; i++) {
-		        for (int j = 0; j < 10; j++) {
-		            cout << " " << arr_country[i][j];
-		            Sleep(90);
-		        }
-		        cout << endl;
-		    }
-		
-		    string guess;
-	        cout << "Press 1 for Guess" << endl;
-		    cout << "Press 2 for see Hint and then guess" << endl;
-		    cout << "Enter input		:	";
-		    cin >> hint;
-		    cout << endl;
-		
-		    if (hint == 1) {
-		        cout << "Enter word you guess         :       ";
-		        cin.ignore();  // Ignore any previous newline character in the input buffer
-		        getline(cin, guess);
-		    }
-		    else if (hint == 2) {
-		        cout << "Guessed word in puzzle size is  :       ";
-		        cout << output.size();
-		        cout << endl;
-		        cout << "Enter word you guess         :       ";
-		        cin.ignore();  // Ignore any previous newline character in the input buffer
-		        getline(cin, guess);
-		    }
-		    else {
-		        cout << "Invalid input	";
-		        return 0;  // Exit the function since the input is invalid
-		    }
-		
-		    cout << endl;
-		    if (c.searchNode(guess)) 
+			//country game
+		    c.countryGame(scores);
+		    category = "country";
+		    cout << "Do you want to stop the game? (y/n): ";
+		    cin >> stop;
+		    if(stop=='Y' || stop=='y')
+		    {
+		    	of.open("text.txt", ios::app);
+		    	of << name << endl;
+		    	of << category << endl;
+		    	of << scores << endl;
+		    	cout << "Progress saved.";
+		    	of.close();
+		    	break;	    	
+			}
+			else if (stop=='N' || stop=='n')
 			{
-		        cout << "Congratulations! You have guessed the correct word." << endl;
-		        scores += 2;
-		    }
-		    else {
-		        cout << "Wrong guess. The correct word was: " << output << endl;
-		    }
-		    cout << endl;
-		}
-		
-		//celebration game
-	    string a_celebration[5] = { "raya","deepavali","ponggal","thaipusam","christmas" };
-	    char arr_celebration[10][10];
-	
-		for(int b=0; b<5; b++)
-		{
-		    for (int i = 0; i < 10; i++) {
-		        for (int j = 0; j < 10; j++) {
-		            char c;
-		            int r;
-		            r = rand() % 26;   // generate a random number
-		            c = 'a' + r;            // Convert to a character from a-z
-		            arr_celebration[i][j] = c;
-		        }
-		    }
-		
-		    int index = rand()%5;
-		    output = a_celebration[index];
-		
-		    if (a_celebration[index].length() % 2 == 0) {
-		        // for even string length case
-		        int row = rand() % 10;
-		        int col = rand() % 3;
-		        for (int i = 0; a_celebration[index][i] != '\0'; i++, col++) {
-		            arr_celebration[row][col] = a_celebration[index][i];
-		        }
-		    }
-		    else {
-		        // for odd string length case
-		        int row = rand() % 3;
-		        int col = rand() % 10;
-		        for (int i = 0; a_celebration[index][i] != '\0'; i++, row++) {
-		            arr_celebration[row][col] = a_celebration[index][i];
-		        }
-		    }
-		
-		    for (int i = 0; i < 10; i++) {
-		        for (int j = 0; j < 10; j++) {
-		            cout << " " << arr_celebration[i][j];
-		            Sleep(90);
-		        }
-		        cout << endl;
-		    }
-		
-		    string guess;
-	        cout << "Press 1 for Guess" << endl;
-		    cout << "Press 2 for see Hint and then guess" << endl;
-		    cout << "Enter input		:	";
-		    cin >> hint;
-		    cout << endl;
-		
-		    if (hint == 1) {
-		        cout << "Enter word you guess         :       ";
-		        cin.ignore();  // Ignore any previous newline character in the input buffer
-		        getline(cin, guess);
-		    }
-		    else if (hint == 2) {
-		        cout << "Guessed word in puzzle size is  :       ";
-		        cout << output.size();
-		        cout << endl;
-		        cout << "Enter word you guess         :       ";
-		        cin.ignore();  // Ignore any previous newline character in the input buffer
-		        getline(cin, guess);
-		    }
-		    else {
-		        cout << "Invalid input	";
-		        return 0;  // Exit the function since the input is invalid
-		    }
-		
-		    cout << endl;
-		    if (cl.searchNode(guess)) 
+				//celebration game
+		    	cl.celebrationGame(scores);	
+		    	category = "celebration";
+		    	cout << "Do you want to stop the game? (y/n): ";
+			    cin >> stop;
+			    if(stop=='Y' || stop=='y')
+			    {
+			    	of.open("text.txt", ios::app);
+			    	of << name << endl;
+			    	of << category << endl;
+			    	of << scores << endl;
+			    	cout << "Progress saved.";
+			    	of.close();
+			    	break;	    	
+				}
+				else if (stop=='N' || stop=='n')
+				{
+					break;
+				}
+				else
+				{
+					cout << "Invalid input";
+					break;
+				}
+			}
+			else 
 			{
-		        cout << "Congratulations! You have guessed the correct word." << endl;
-		        scores += 2;
-		    }
-		    else {
-		        cout << "Wrong guess. The correct word was: " << output << endl;
-		    }
-		    cout << endl;
+				cout << "Invalid input";
+				break;
+			}			
 		}
-		
+		else
+		{ 
+			cout << "Invalid input";
+			break;
+		}
 	}
 	
+	//resume game
+	while(newOrResume=='n' || newOrResume=='N')
+	{
+		cout << "Enter your previous category(food, country, celebration): ";
+		cin >> category;
+		if (category=="food"||category=="FOOD"||category=="Food")
+		{
+			F.open("text.txt");
+			while(F)
+			{
+				getline(F,line);
+				if(line==name)
+				{
+					cout << "Name: " << line << endl;
+					getline(F,line);
+					if(line==category)
+					{
+						cout << "Category: " << line << endl;
+						getline(F,line);
+						cout << "Scores: " << line << endl;
+						cout << "Your next kingdom is Country" << endl;
+						//country game
+					    c.countryGame(scores);
+					    string category = "country";
+					    cout << "Do you want to stop the game? (y/n): ";
+					    cin >> stop;
+					    if(stop=='Y' || stop=='y')
+					    {
+					    	of.open("text.txt", ios::app);
+					    	of << name << endl;
+					    	of << category << endl;
+					    	of << scores << endl;
+					    	cout << "Progress saved.";
+					    	of.close();
+					    	break;	    	
+						}
+						else if (stop=='N' || stop=='n')
+						{
+							//celebration game
+					    	cl.celebrationGame(scores);	
+					    	string category = "celebration";
+					    	cout << "Do you want to stop the game? (y/n): ";
+						    cin >> stop;
+						    if(stop=='Y' || stop=='y')
+						    {
+						    	of.open("text.txt", ios::app);
+						    	of << name << endl;
+						    	of << category << endl;
+						    	of << scores << endl;
+						    	cout << "Progress saved.";
+						    	of.close();
+						    	break;	    	
+							}
+							else if (stop=='N' || stop=='n')
+							{
+								break;
+							}
+							else
+							{
+								cout << "Invalid input";
+								break;
+							}
+						}
+						else 
+						{
+							cout << "Invalid input";
+							break;
+						}			
+					}
+				}
+				
+			}
+			F.close();
+		}
+		else if(category=="country"||category=="COUNTRY"||category=="Country")
+		{
+			F.open("text.txt");
+			while(F)
+			{
+				getline(F,line);
+				if(line==name)
+				{
+					cout << "Name: " << line << endl;
+					getline(F,line);
+					if(line==category)
+					{
+						cout << "Category: " << line << endl;
+						getline(F,line);
+						cout << "Scores: " << line << endl;
+						cout << "Your next kingdom is Celebration" << endl;
+						//celebration game
+					    cl.celebrationGame(scores);	
+					    string category = "celebration";
+					    cout << "Do you want to stop the game? (y/n): ";
+						cin >> stop;
+						if(stop=='Y' || stop=='y')
+						{
+						of.open("text.txt", ios::app);
+						of << name << endl;
+						of << category << endl;
+						of << scores << endl;
+						cout << "Progress saved.";
+						of.close();
+						break;	    	
+						}
+						else if (stop=='N' || stop=='n')
+						{
+							break;
+						}
+						else
+						{
+							cout << "Invalid input";
+							break;
+						}
+					}			
+				}
+			}
+			F.close();
+		}
+		else if(category=="celebration"||category=="CELEBRATION"||category=="Celebration")
+		{
+			F.open("text.txt");
+			while(F)
+			{
+				getline(F,line);
+				if(line==name)
+				{
+					cout << "Name: " << line << endl;
+					getline(F,line);
+					if(line==category)
+					{
+						cout << "Category: " << line << endl;
+						getline(F,line);
+						cout << "Scores: " << line << endl;
+					}
+				}
+			}
+			F.close();
+		}
+		else
+		{
+			cout << "Invalid input";
+			break;
+		}			
+	}
 	cout << "Your total score is: " << scores << endl;
+	
+	if (scores>=25)
+	{
+		cout << "You have scored enough to save the princess!! Congratulations " << name << ", you're a true warrior!" << endl;
+		cout << "The doors to the tower is now opened." << endl;
+		cout << "Go back with the princess and have a good rest champ!" << endl;;
+	}
+	else
+	{
+		cout << "OH NOO!!! You score is insufficient to save the princess." << endl;
+		cout << "The doors to the tower is now locked. You need to leave without the princess." << endl;
+	}
 	return 0;
 }
