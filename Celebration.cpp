a790da0dce938f28dbf3e629bd6023382cc24ab7
Jashh18 <118554140+Jashh18@users.noreply.github.com>
2023-06-27 03:35:38 +0800

    Update Celebration.cpp

--- Celebration.cpp
+++ Celebration.cpp
@@ -1,4 +1,5 @@
 #include "Celebration.h"
+#include <windows.h>
 #include <iostream>
 
 using namespace std;
@@ -57,4 +58,88 @@ bool CelebrationList::searchNode(string guess)
 	}
 	return false;
 	
+}
+
+void CelebrationList::celebrationGame(int& scores)
+{
+	string a_celebration[5] = { "raya","deepavali","ponggal","thaipusam","christmas" };
+	    char arr_celebration[10][10];
+		int cl_index = 0;
+		for(int b=0; b<5; b++)
+		{
+		    for (int i = 0; i < 10; i++) {
+		        for (int j = 0; j < 10; j++) {
+		            char c;
+		            int r;
+		            r = rand() % 26;   // generate a random number
+		            c = 'a' + r;            // Convert to a character from a-z
+		            arr_celebration[i][j] = c;
+		        }
+		    }
+		
+		    string output = a_celebration[cl_index];
+		
+		    if (a_celebration[cl_index].length() % 2 == 0) {
+		        // for even string length case
+		        int row = rand() % 10;
+		        int col = rand() % 3;
+		        for (int i = 0; a_celebration[cl_index][i] != '\0'; i++, col++) {
+		            arr_celebration[row][col] = a_celebration[cl_index][i];
+		        }
+		    }
+		    else {
+		        // for odd string length case
+		        int row = rand() % 3;
+		        int col = rand() % 10;
+		        for (int i = 0; a_celebration[cl_index][i] != '\0'; i++, row++) {
+		            arr_celebration[row][col] = a_celebration[cl_index][i];
+		        }
+		    }
+		
+		    for (int i = 0; i < 10; i++) {
+		        for (int j = 0; j < 10; j++) {
+		            cout << " " << arr_celebration[i][j];
+		            Sleep(90);
+		        }
+		        cout << endl;
+		    }
+		
+		    string guess;
+		    int hint;
+	        cout << "Press 1 for Guess" << endl;
+		    cout << "Press 2 for see Hint and then guess" << endl;
+		    cout << "Enter input		:	";
+		    cin >> hint;
+		    cout << endl;
+		
+		    if (hint == 1) {
+		        cout << "Enter word you guess         :       ";
+		        cin.ignore();  // Ignore any previous newline character in the input buffer
+		        getline(cin, guess);
+		    }
+		    else if (hint == 2) {
+		        cout << "Guessed word in puzzle size is  :       ";
+		        cout << output.size();
+		        cout << endl;
+		        cout << "Enter word you guess         :       ";
+		        cin.ignore();  // Ignore any previous newline character in the input buffer
+		        getline(cin, guess);
+		    }
+		    else {
+		        cout << "Invalid input	";
+		        break; // Exit the function since the input is invalid
+		    }
+		
+		    cout << endl;
+		    if (searchNode(guess)) 
+			{
+		        cout << "Congratulations! You have guessed the correct word." << endl;
+		        scores += 2;
+		    }
+		    else {
+		        cout << "Wrong guess. The correct word was: " << output << endl;
+		    }
+		    cout << endl;
+		    cl_index++;
+		}		
 }
