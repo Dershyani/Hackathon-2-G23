b384916185a33a348c471952634e7cfb8061e44f
Dershyani <121543719+Dershyani@users.noreply.github.com>
2023-06-28 11:32:54 +0800

    Update MainHack.cpp

--- MainHack.cpp
+++ MainHack.cpp
@@ -3,10 +3,10 @@
 #include <limits>
 #include <windows.h>
 #include <string>
-#include "Food.h"
-#include "Country.h"
-#include "Celebration.h"
-#include "Chapter.h"
+#include "FoodL.h"
+#include "CountryL.h"
+#include "CelebrationL.h"
+#include "ChapterL.h"
 
 using namespace std;
 
@@ -26,16 +26,17 @@ int main()
 	
 	//start game
 	ch.startGame();
-	cout << "What is your name warrior: ";
+	cout << "What is your name warrior: "; //enter name
 	getline(cin, name);
 	cout << "Press any key to continue" << endl;
 	cin.ignore();
-	cout << "Do you want to play a new game or resume a game(n/r): ";
+	cout << "Do you want to play a new game or resume a game(n/r): "; //to start a new game or resume game
 	cin >> newOrResume;
 	
 	
 	while(newOrResume == 'n' || newOrResume == 'N')
 	{
+		//appending words into linked lists
 		//food
 		f.appendNode("apple");
 		f.appendNode("chicken");
@@ -58,13 +59,14 @@ int main()
 		cl.appendNode("diwali");
 		
 		//food game
-		ch.foodIntro();
-	    f.foodGame(scores);
+		ch.foodIntro(); //from chapter class for the intro
+	    f.foodGame(scores); //from food class to play the game
 	    category = "food";
-	    cout << "Do you want to stop the game? (y/n): ";
+	    cout << "Do you want to stop the game? (y/n): "; //to stop the game
 	    cin >> stop;
 	    if(stop=='y' || stop=='Y')
 	    {
+	    	//writes to a text file
 	    	of.open("text.txt", ios::app);
 	    	of << name << endl;
 	    	of << category << endl;
@@ -76,13 +78,14 @@ int main()
 		else if(stop=='n' || stop=='N')
 		{
 			//country game
-			ch.countryIntro();
-		    c.countryGame(scores);
+			ch.countryIntro(); //from chapter class for intro
+		    c.countryGame(scores); //from country class to play game
 		    category = "country";
-		    cout << "Do you want to stop the game? (y/n): ";
+		    cout << "Do you want to stop the game? (y/n): "; //to stop the game
 		    cin >> stop;
 		    if(stop=='y' || stop=='Y')
 		    {
+		    	//writes into a text file
 		    	of.open("text.txt", ios::app);
 		    	of << name << endl;
 		    	of << category << endl;
@@ -94,13 +97,14 @@ int main()
 			else if (stop=='n' || stop=='N')
 			{
 				//celebration game
-				ch.celebrationIntro();
-		    	cl.celebrationGame(scores);	
+				ch.celebrationIntro(); //from chapter class for intro
+		    	cl.celebrationGame(scores);	//from celebration class to play the game
 		    	category = "celebration";
-		    	cout << "Do you want to stop the game? (yes/no): ";
+		    	cout << "Do you want to stop the game? (yes/no): "; //to stop the game
 			    cin >> stop;
 			    if(stop=='y' || stop=='Y')
 			    {
+			    	//writes into a text file
 			    	of.open("text.txt", ios::app);
 			    	of << name << endl;
 			    	of << category << endl;
@@ -111,14 +115,14 @@ int main()
 				}
 				else if (stop=='n' || stop=='N')
 				{
-					cout << "Your total score is: " << scores << endl;
+					cout << "Your total score is: " << scores << endl; //shows score
 					if (scores>=25)
 					{
-						ch.winGame();
+						ch.winGame(); //text if won
 					}
 					else
 					{
-						ch.loseGame();
+						ch.loseGame(); //text if lose
 					}
 					break;
 				}
@@ -144,32 +148,33 @@ int main()
 	//resume game
 	while(newOrResume=='r' || newOrResume=='R')
 	{
-		cout << "Enter your previous category(food, country, celebration): ";
+		cout << "Enter your previous category(food, country, celebration): "; //to find category
 		cin >> category;
 		if (category=="food"||category=="FOOD"||category=="Food")
 		{
 			F.open("text.txt");
 			while(F)
 			{
-				getline(F,line);
+				getline(F,line); //if same name
 				if(line==name)
 				{
-					cout << "Name: " << line << endl;
-					getline(F,line);
-					if(line==category)
+					cout << "Name: " << line << endl; //output name
+					getline(F,line); 
+					if(line==category) //if same category
 					{
-						cout << "Category: " << line << endl;
+						cout << "Category: " << line << endl; //output category
 						getline(F,line);
 						cout << "Scores: " << line << endl;
-						cout << "Your next kingdom is Country" << endl;
+						cout << "Your next kingdom is Country" << endl; //goes to next kingdom
 						//country game
 						ch.countryIntro();
 					    c.countryGame(scores);
 					    string category = "country";
-					    cout << "Do you want to stop the game? (y/n): ";
+					    cout << "Do you want to stop the game? (y/n): "; //stop game
 					    cin >> stop;
 					    if(stop=='y' || stop=='Y')
 					    {
+					    	//writes into text file
 					    	of.open("text.txt", ios::app);
 					    	of << name << endl;
 					    	of << category << endl;
@@ -181,13 +186,14 @@ int main()
 						else if (stop=='n' || stop=='N')
 						{
 							//celebration game
-							ch.celebrationIntro();
-					    	cl.celebrationGame(scores);	
+							ch.celebrationIntro(); //from chapter class for intro
+					    	cl.celebrationGame(scores);	//from celebration class to play game
 					    	string category = "celebration";
-					    	cout << "Do you want to stop the game? (y/n): ";
+					    	cout << "Do you want to stop the game? (y/n): "; // to stop
 						    cin >> stop;
 						    if(stop=='y' || stop=='Y')
 						    {
+						    	//writes into text file
 						    	of.open("text.txt", ios::app);
 						    	of << name << endl;
 						    	of << category << endl;
@@ -223,43 +229,44 @@ int main()
 			while(F)
 			{
 				getline(F,line);
-				if(line==name)
+				if(line==name) //same name
 				{
-					cout << "Name: " << line << endl;
+					cout << "Name: " << line << endl; //output name
 					getline(F,line);
-					if(line==category)
+					if(line==category) //same category
 					{
-						cout << "Category: " << line << endl;
+						cout << "Category: " << line << endl; //output category
 						getline(F,line);
 						cout << "Scores: " << line << endl;
 						cout << "Your next kingdom is Celebration" << endl;
 						
 						//celebration game
-						ch.celebrationIntro();
-					    cl.celebrationGame(scores);	
+						ch.celebrationIntro(); //from chapter class for intro	
+					    cl.celebrationGame(scores);	//from celebration class to play game
 					    string category = "celebration";
-					    cout << "Do you want to stop the game? (y/n): ";
+					    cout << "Do you want to stop the game? (y/n): "; //to stop
 						cin >> stop;
 						if(stop=='y' || stop=='Y')
 						{
-						of.open("text.txt", ios::app);
-						of << name << endl;
-						of << category << endl;
-						of << scores << endl;
-						cout << "Progress saved.";
-						of.close();
-						break;	    	
+							//writes into textfile
+							of.open("text.txt", ios::app);
+							of << name << endl;
+							of << category << endl;
+							of << scores << endl;
+							cout << "Progress saved.";
+							of.close();
+							break;	    	
 						}
 						else if (stop=='n' || stop=='N')
 						{
-							cout << "Your total score is: " << scores << endl;
+							cout << "Your total score is: " << scores << endl; //score
 							if (scores>=25)
 							{
-								ch.winGame();
+								ch.winGame();//text if win
 							}
 							else
 							{
-								ch.loseGame();
+								ch.loseGame();//text if lose
 							}
 							break;
 						}
@@ -279,23 +286,23 @@ int main()
 			while(F)
 			{
 				getline(F,line);
-				if(line==name)
+				if(line==name) //same name
 				{
-					cout << "Name: " << line << endl;
+					cout << "Name: " << line << endl; //output name
 					getline(F,line);
-					if(line==category)
+					if(line==category) //same category
 					{
-						cout << "Category: " << line << endl;
+						cout << "Category: " << line << endl; //output category
 						getline(F,line);
 						cout << "Scores: " << line << endl;
-						cout << "Your total score is: " << scores << endl;
+						cout << "Your total score is: " << scores << endl; //score
 						if (scores>=25)
 						{
-							ch.winGame();
+							ch.winGame(); //text if win
 						}
 						else
 						{
-							ch.loseGame();
+							ch.loseGame();//text if lose
 						}
 					}
 				}
